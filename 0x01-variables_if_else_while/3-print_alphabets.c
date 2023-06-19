a7640d4cc3c0d8667569f51cf6bf29afcfc60523
Olivera <oliverawilson360@gmail.com>
2023-06-19 02:51:13 -0700

    three

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+  *main - Point
+  *Description 'Alphabets'
+  *Return: always 0
+*/
+int main(void)
+{
+int n = 97;
+int n = 65;
+
+while (n <= 122)
+{
+putchar(n);
+n++;
+}
+while (m <= 90)
+{
+putchar(m);
+m++;
+}
+putchar('\n');
+return (0);
+}
