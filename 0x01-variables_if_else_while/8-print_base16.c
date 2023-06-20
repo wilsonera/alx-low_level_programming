9e453de4759fddb3e456629828e44eb6480c5e8b
Olivera <oliverawilson360@gmail.com>
2023-06-19 18:41:22 -0700

    again

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - ready
+*Description 'doing hard things'
+*Return: always 0
+*/
+int main(void)
+{
+int n;
+int m;
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+}
+for (m = 97; m <= 102; m++)
+{
+putchar(m);
+}
+return (0);
+}
