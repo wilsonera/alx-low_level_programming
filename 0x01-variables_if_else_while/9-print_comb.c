cebb1f50feacc6ffa73f9fdc5e563a24ce2005a6
Olivera <oliverawilson360@gmail.com>
2023-06-19 19:48:04 -0700

    done

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,19 @@
-:wq
+#include <stdio.h>
+/**
+*main - wow
+*Description 'noise'
+*Return: always 0
+*/
+int main(void)
+{
+int n;
+
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+putchar(',');
+putchar(',');
+}
+putchar('\n');
+return (0);
+}
