eb32b8336d736064c34377b7d7e643d9f9c26061
Olivera <oliverawilson360@gmail.com>
2023-06-19 18:05:46 -0700

    night

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - numbers
+*Description 'Print numbers'
+*Return: always 0
+*/
+int main(void)
+{
+int n;
+
+for (n = 0; n <= 9; n++)
+{
+putchar(n + 48);
+}
+return (0);
+}
