64d05b789e1c7a4362c7cb774f4d23534f73984e
Olivera <oliverawilson360@gmail.com>
2023-06-22 19:13:20 -0700

    three

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,11 +5,12 @@
  */
 void print_numbers(void)
 {
-int c;
-for (c = 0; c <= 9; c++)
-{
-_putchar(c + '0');
+char c;
+
+for (c = '0'; c <= '9'; c++)
+i{
+if (!(c == '2' || c == '4'))
+_putchar(c);
 }
 _putchar('\n');
-return (0);
 }
