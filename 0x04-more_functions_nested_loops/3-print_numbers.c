ba186bdff7b79ab70068e898a63e329adc90de05
Olivera <oliverawilson360@gmail.com>
2023-06-22 19:58:15 -0700

    three

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,18 @@
 #include "main.h"
+
 /**
- * print_numbers - print 0-9
- * return: void
- */
+*print_numbers - prints 0-0
+*
+*Return: void
+*/
+
 void print_numbers(void)
 {
-char c;
+	char c;
 
 for (c = '0'; c <= '9'; c++)
 {
-if (!(c == '2' || c == '4'))
 _putchar(c);
-}
+	}
 _putchar('\n');
 }
